432ba072977c887f59386fa3c629dd3a542b57b3
zibon07 <shorifhussainzibon@gmail.com>
2026-01-21 16:03:27 +0600

    navbar added

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+              <li><a>Home</a></li>
+              <li><a>FAQ</a></li>
+              <li><a>Changelog</a></li>
+              <li><a>Blog</a></li>
+              <li><a>Download</a></li>
+              <li><a>Contact</a></li>
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-2xl pl-10 font-bold ">CS — Ticket System</a>
+        </div>
+        <div className='flex items-center'>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
+              <li><a>Home</a></li>
+              <li><a>FAQ</a></li>
+              <li><a>Changelog</a></li>
+              <li><a>Blog</a></li>
+              <li><a>Download</a></li>
+              <li><a>Contact</a></li>
+            </ul>
+          </div>
+          <div className="ml-10">
+            <a className="btn bg-linear-to-r from-[#632EE3] to-[#9F62F2] text-white">+ New Ticket</a>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default Navbar;
